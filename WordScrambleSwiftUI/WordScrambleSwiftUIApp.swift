5e103978339524659728277eb40f98b6f2f90d58
Anastasiia Veremiichyk <anastasia.veremi@gmail.com>
2022-09-11 22:04:06 +0200

    Project set up.

---
+++ WordScrambleSwiftUI/WordScrambleSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleSwiftUIApp.swift
+//  WordScrambleSwiftUI
+//
+//  Created by Anastasiia Veremiichyk on 11/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
